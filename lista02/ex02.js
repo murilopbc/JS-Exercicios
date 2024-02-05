85b489fe3e9fc37584870b3dd9366fcf666207db
Murilo Poltronieri Batista De Campos <49900491874@>
2024-02-05 13:56:28 -0300

    js

---
+++ lista02/ex02.js
@@ -0,0 +1,5 @@
+/*
+[do..while] Desenvolva uma página que leia as idades de “n” pessoas até que a soma das idades 
+   ultrapasse 100 anos. Ao término, apresente:
+   -> A idade da pessoa mais velha.
+*/
