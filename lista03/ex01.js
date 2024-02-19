fb512f090a186ec0fb9e642a73d44aa17086ff15
Murilo Poltronieri Batista De Campos <49900491874@>
2024-02-19 15:39:10 -0300

    js

--- lista03/ex01.js
+++ lista03/ex01.js
@@ -12,3 +12,51 @@ Cada opção será implementada em uma função.
 
 */
 
+// Função Média
+
+function media(num1, num2){
+    return (num1 + num2) / 2 
+}
+console.log(media(10, 20))
+
+// Função Menor Número
+
+function menor(num1, num2){
+    if (num1 > num2) {
+        return num2
+    }
+    else {
+        return num1
+    }
+}
+console.log(menor(11,12))
+
+// Função diferença entre números
+
+function diferenca(num1, num2){
+    if (num1 > num2) {
+        return num1 - num2
+    }
+    else {
+        return num2 - num1
+    }
+}
+console.log(diferenca(5,40))
+
+// Função números em ordem crescente
+
+function crescente(num1, num2){
+    if (num1 > num2) {
+        let temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+    return [num1, num2]
+
+}
+console.log(crescente(44,42))
+
+
+
+
+
